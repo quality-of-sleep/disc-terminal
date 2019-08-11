db09e91b87d729fea1c2a5195fcd4a8b3ee25204
zuka-e <nq.iizuka@gmail.com>
2019-08-11 04:39:08 +0000

    test

---
+++ db/migrate/20190811043717_create_order_details.rb
@@ -0,0 +1,13 @@
+class CreateOrderDetails < ActiveRecord::Migration[5.2]
+  def change
+    create_table :order_details do |t|
+      t.references :order, foreign_key: true
+      t.references :item, foreign_key: true
+      t.string :item
+      t.string :artist
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
