d268c1380679bbc4daa625f2f9251c33a15c6137
TBTYOF <toribatyof@gmail.com>
2019-08-12 00:47:50 +0900

    [Add]items test

--- app/controllers/users/items_controller.rb
+++ app/controllers/users/items_controller.rb
@@ -2,4 +2,15 @@ class Users::ItemsController < ApplicationController
 	def index
 		@items = Item.all
 	end
+	private
+	def item_params
+		params.require(:item).permit(
+			:artist_id,
+			:genre_id,
+			:label_id,
+			:name,
+			:price,
+			:sales_status,
+			:stock)
+	end
 end
