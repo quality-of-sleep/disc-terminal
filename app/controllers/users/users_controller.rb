1f40cd897b1b7185d3f258633829794ceeeb9226
TBTYOF <toribatyof@gmail.com>
2019-08-12 23:57:00 +0900

    [Add]carts-views link_to item/show [Remove]destroy-action > user.find

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -24,8 +24,8 @@ class Users::UsersController < ApplicationController
 
 
 	# tori_cart-test(カート機能試すときはコメントアウト外してください)
-# 	private
-# 	def user_params
-# 		params.require(:user).permit(:email)
-# 	end
-# end
+	private
+	def user_params
+		params.require(:user).permit(:email)
+	end
+end
