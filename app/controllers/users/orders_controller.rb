7cf354c1420c85a85e73d37659c8b2ebf6717539
TBTYOF <toribatyof@gmail.com>
2019-08-14 16:21:50 +0900

    add form_for

--- app/controllers/users/orders_controller.rb
+++ app/controllers/users/orders_controller.rb
@@ -1,4 +1,21 @@
 class Users::OrdersController < ApplicationController
+	# carts_helper呼び出し
+	include Users::CartsHelper
+
 	def new
+		@user = User.find(params[:user_id])
+		@order = Order.new
+		@cart = Cart.new
+		# 小計Helper
+		price = price_reckoning(@user.carts)
+		# 小計消費税helper
+		@tax = tax(price)
+		@total_price = on_tax_price(price)
+		# 小計個数helper
+		@total_amount = total_amount(@user.carts)
+	end
+
+	def create
+		
 	end
 end
