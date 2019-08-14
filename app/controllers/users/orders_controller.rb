fde0bc25a280f167558a45fdd88a1934f7adb6b3
TBTYOF <toribatyof@gmail.com>
2019-08-14 16:55:11 +0900

    [Add] address-form to order-view

--- app/controllers/users/orders_controller.rb
+++ app/controllers/users/orders_controller.rb
@@ -5,7 +5,7 @@ class Users::OrdersController < ApplicationController
 	def new
 		@user = User.find(params[:user_id])
 		@order = Order.new
-		@cart = Cart.new
+
 		# 小計Helper
 		price = price_reckoning(@user.carts)
 		# 小計消費税helper
@@ -13,6 +13,18 @@ class Users::OrdersController < ApplicationController
 		@total_price = on_tax_price(price)
 		# 小計個数helper
 		@total_amount = total_amount(@user.carts)
+
+		# テスト用コード
+		@cart = Cart.new
+		@postal_code = "1500041" # 現住所
+		@address = "東京都渋谷区神南1丁目19番11号パークウェースクエア2 4階"
+		@telephone_number = "0368694700"
+
+		@delivery_User_name = "海馬瀬人"# 配送先
+		@delivery_postal_code = "5420076"
+		@details = "大阪府大阪市中央区難波4丁目7-14難波フロントビル 4階"
+		@delivery_telephone_number = "0452224444"
+
 	end
 
 	def create
