cfe845ff6ddb800fe7f0fb174c592bb35e01f434
TBTYOF <toribatyof@gmail.com>
2019-08-16 12:52:35 +0900

    Add controller

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -1,2 +1,9 @@
 class Admins::OrdersController < ApplicationController
+	include Users::OrdersHelper
+	def show
+		@user = User.find(params[:user_id])
+		@order = Order.find(params[:id])
+		@discount = 0
+		@total_amount = total_amount(@order.order_details)
+	end
 end
