dc9bc94def3f0960ba817d2a584e4b0b590c18a7
TBTYOF <toribatyof@gmail.com>
2019-08-16 21:23:58 +0900

    comment out

--- app/controllers/users/items_controller.rb
+++ app/controllers/users/items_controller.rb
@@ -4,9 +4,9 @@ class Users::ItemsController < ApplicationController
 		@items =Item.all
 	end
 
-	# def show
+	def show
 		
-	# end
+	end
 
 
 
@@ -53,20 +53,20 @@ class Users::ItemsController < ApplicationController
 
 
 	# tori_cart-test(カート機能試すときはコメントアウト外してください)
-	def show
-		@user = current_user
-		@item = Item.find(params[:id])
-		@cart = Cart.new
-	end
-	private
-	def item_params
-		params.require(:item).permit(
-			:artist_id,
-			:genre_id,
-			:label_id,
-			:name,
-			:price,
-			:sales_status,
-			:stock)
-	end
+	# def show
+	# 	@user = current_user
+	# 	@item = Item.find(params[:id])
+	# 	@cart = Cart.new
+	# end
+	# private
+	# def item_params
+	# 	params.require(:item).permit(
+	# 		:artist_id,
+	# 		:genre_id,
+	# 		:label_id,
+	# 		:name,
+	# 		:price,
+	# 		:sales_status,
+	# 		:stock)
+	# end
 end
