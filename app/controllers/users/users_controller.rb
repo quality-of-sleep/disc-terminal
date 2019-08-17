04deaa71a1469b91879819f89e2fdcfd1174c547
yuya2413 <klx250cbr250@gmail.com>
2019-08-17 04:14:10 +0000

    Add users#show

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -1,38 +1,25 @@
 class Users::UsersController < ApplicationController
 
-	def show
-		
-	end
+  def show
+  	@user = User.find(params[:id])
+  end
 
-  
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+  def update
+  end
+
 
   def withdrawal
   end
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 	# tori_cart-test(カート機能試すときはコメントアウト外してください)
-	# private
-	# def user_params
-	# 	params.require(:user).permit(:email)
-	# end
+ 	private
+ 	def user_params
+ 		params.require(:user).permit(:email, :last_name, :first_name, :last_name_kana, :first_name_kana, :telephone_number, :postal_code, :address, :payment)
+ 	end
 end
