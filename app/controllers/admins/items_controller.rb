56adcd5a90cc5a4091e4af03584582d42ea7758e
zuka-e <nq.iizuka@gmail.com>
2019-08-17 19:12:31 +0000

    Add checkbox for input case and select case

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -6,19 +6,19 @@ class Admins::ItemsController < ApplicationController
 		@artists = Artist.all
 		@genres = Genre.all
 		@items = Item.page(params[:page])
-		@items = @items.search(key: 'name', value: params[:search]) if params[:search] != nil
+		@items = @items.search(key: 'name', value: params[:search]) if params[:search] != nil # == !p.nil?
 		@items = @items.where(["artist_id = ?","#{params[:artist]}"]) if params[:artist].present?
 		@items = @items.where(["genre_id = ?", "#{params[:genre]}"]) if params[:genre].present?
 		@items = @items.reorder(["?", "#{params[:key]} #{params[:direction]}"])
-	#injection
+
 	end
 	def new
 		@artist = Artist.new
 		@genre = Genre.new
 		@label = Label.new
 		@item = Item.new
-			@disc = @item.discs.build # number
-				@songs = @disc.songs.build # number name
+		@disc = @item.discs.build
+		@songs = @disc.songs.build
 
 	end
 	def create
@@ -26,13 +26,16 @@ class Admins::ItemsController < ApplicationController
 		@genre = Genre.new(genre_params)
 		@label = Label.new(label_params)
 		@item = Item.new(item_params)
-		@item.artist = @artist
-		@item.genre = @genre
-		@item.label = @label
-		@item.save
-		render 'new'
-		debugger
-
+		# チェックボックスをチェックしフォームを入力した場合のみ代入する
+		@item.artist = @artist if params[:artist?] && params[:artist][:name]
+		@item.genre = @genre if params[:genre?] && params[:genre][:name]
+		@item.label = @label if params[:label?] && params[:label][:name]
+		if @item.save
+			render 'show'
+			debugger
+		else
+			redirect_to new_admins_item_url
+		end
 	end
 	def show
 	end
@@ -51,12 +54,15 @@ class Admins::ItemsController < ApplicationController
 		def label_params
 			params.require(:label).permit(:name)
 		end
-	  def item_params
-	    params.require(:item).permit(:name, :price, :stock, :sales_status, :image,
 
-	    			discs_attributes: [:id, :number, :_destroy,
-	    				songs_attributes: [:id, :name, :number, :_destroy]
-	    			],
+		# nested_formによって生成された *_attributesも受け取る
+	  def item_params
+	    params.require(:item).permit(
+	    	:name, :price, :stock, :sales_status, :image,
+	    	:artist_id, :genre_id, :label_id,
+				discs_attributes: [:id, :number, :_destroy,
+	    		songs_attributes: [:id, :name, :number, :_destroy]
+	    	]
 
 	   	)
 	  end
