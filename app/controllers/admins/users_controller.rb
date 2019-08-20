af11cacd11a2130871dcad93b1249138d3686d2e
yuya2413 <klx250cbr250@gmail.com>
2019-08-20 04:39:52 +0000

    Finish admin_comments admins/users#show

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -7,6 +7,7 @@ class Admins::UsersController < ApplicationController
 
 	def show
 	  	@user = User.find(params[:id])
+	    @admin_comment = AdminComment.new
 	end
 
     def quit
@@ -18,7 +19,7 @@ class Admins::UsersController < ApplicationController
 
  	private
  	def user_params
- 		params.require(:user).permit(:id, :email, :last_name, :first_name, :last_name_kana, :first_name_kana, :telephone_number, :postal_code, :address, :payment, :is_quit, delivery_addresses_attributes: [:id, :recipient, :postal_code, :details, :telephone_number, :_destroy], admin_comments_attributes: [:id, :body, :destroy])
+ 		params.require(:user).permit(:id, :email, :last_name, :first_name, :last_name_kana, :first_name_kana, :telephone_number, :postal_code, :address, :payment, :is_quit, delivery_addresses_attributes: [:id, :recipient, :postal_code, :details, :telephone_number, :_destroy], admin_comments_attributes: [:id, :body, :_destroy])
  	end
 end
 
